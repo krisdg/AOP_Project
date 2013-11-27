e283d04b25c2edf3acc166b52a9b1c8bc8090f5c
michielbijlsma <michieb36@hotmail.com>
2013-11-27 11:26:56 +0100

    Michiel is de beste!!

--- TCMIS/src/tcmis/mainpackage/Station.java
+++ TCMIS/src/tcmis/mainpackage/Station.java
@@ -16,7 +16,7 @@ public class Station extends Agent {
   protected void setup() { 
     // Printout a welcome message
     System.out.println("Hello World. I am an agent!");
-    System.out.println("Hello World. I am an agent!");
+    System.out.println("Hello World. I am an agent! En Michiel is de beste!!");
     System.out.println("My local-name is "+getAID().getLocalName());
     System.out.println("My GUID is "+getAID().getName());
     System.out.println("My addresses are:");

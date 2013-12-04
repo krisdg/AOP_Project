ca3ee40db891972a5f9e1af146c9e9acfdae0743
JC de Groot <kris.de.groot@hva.nl>
2013-12-04 11:53:59 +0100

    Monitor added

---
+++ TCMIS/src/tcmis/mainpackage/Monitor.java
@@ -0,0 +1,84 @@
+package tcmis.mainpackage;
+
+/**
+ * Section 4.1, Page 51
+ * Creating a JADE agent is as simple as defining a class that extends the jade.core.Agent 
+ * class and implementing the setup() method as exemplified in the code below.
+ **/
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jade.core.AID;
+import jade.domain.AMSService;
+import jade.domain.FIPAAgentManagement.AMSAgentDescription;
+import jade.domain.FIPAAgentManagement.SearchConstraints;
+import jade.gui.GuiAgent;
+import jade.gui.GuiEvent;
+
+public class Monitor extends GuiAgent {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	transient protected MonitorGUI gui;
+
+	// These constants are used by the Gui to post Events to the Agent
+	public static final int EXIT = 1000;
+	public static final int MOVE_EVENT = 1001;
+	public static final int STOP_EVENT = 1002;
+	public static final int CONTINUE_EVENT = 1003;
+	public static final int REFRESH_EVENT = 1004;
+	public static final int CLONE_EVENT = 1005;
+
+	public void setup() {
+		// creates and shows the GUI
+		gui = new MonitorGUI(this);
+		gui.setVisible(true);
+		
+		
+	}
+	
+	public String[] getAgents() {
+		AMSAgentDescription[] AMSAgents = null;
+		try {
+			SearchConstraints c = new SearchConstraints();
+			c.setMaxResults(new Long(-1));
+			AMSAgents = AMSService.search(this, new AMSAgentDescription(), c);
+		} catch (Exception e) {
+			System.out.println("Problem searching AMS: " + e);
+			e.printStackTrace();
+		}
+
+		String[] agents = new String[AMSAgents.length];
+		
+		AID myID = getAID();
+		for (int i = 0; i < AMSAgents.length; i++) {
+			AID agentID = AMSAgents[i].getName();
+
+			agents[i] = agentID.getName();
+		}
+		
+		return agents;
+	}
+
+	// AGENT OPERATIONS FOLLOWING GUI EVENTS
+	protected void onGuiEvent(GuiEvent ev) {
+		switch (ev.getType()) {
+		case EXIT:
+			break;
+		case MOVE_EVENT:
+			break;
+		case CLONE_EVENT:
+			break;
+		case STOP_EVENT:
+			break;
+		case CONTINUE_EVENT:
+			break;
+		case REFRESH_EVENT:
+			break;
+		}
+
+	}
+
+}
---
+++ TCMIS/src/tcmis/mainpackage/MonitorGUI.java
@@ -0,0 +1,217 @@
+package tcmis.mainpackage;
+
+/**
+ * Section 4.1, Page 51
+ * Creating a JADE agent is as simple as defining a class that extends the jade.core.Agent 
+ * class and implementing the setup() method as exemplified in the code below.
+ **/
+
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.WindowEvent;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+
+import jade.core.Agent;
+import jade.core.AID;
+import jade.domain.AMSService;
+import jade.domain.FIPAAgentManagement.*;
+
+public class MonitorGUI extends JFrame {
+
+	Monitor parentAgent;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	// GUI Objects
+    private javax.swing.JButton btn_openAgent;
+    private javax.swing.JButton btn_refreshAgentList;
+    private javax.swing.JComboBox cmb_agentList;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+
+	MonitorGUI(Monitor parentAgent) {
+		this.parentAgent = parentAgent;
+
+		initComponents();
+
+		refreshAgents();
+	}
+
+	private void initComponents() {
+		// <editor-fold defaultstate="collapsed" desc="Generated Code">
+
+		jLabel1 = new javax.swing.JLabel();
+		cmb_agentList = new javax.swing.JComboBox();
+		btn_refreshAgentList = new javax.swing.JButton();
+		btn_openAgent = new javax.swing.JButton();
+		jPanel1 = new javax.swing.JPanel();
+		jPanel2 = new javax.swing.JPanel();
+
+		setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+
+		jLabel1.setText("Select Agent:");
+
+		cmb_agentList.setModel(new javax.swing.DefaultComboBoxModel(
+				new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+
+		btn_refreshAgentList.setText("Refresh");
+		btn_refreshAgentList.setActionCommand("R");
+		btn_refreshAgentList.setName("btn_refreshAgentList"); // NOI18N
+		btn_refreshAgentList
+				.addActionListener(new java.awt.event.ActionListener() {
+					public void actionPerformed(java.awt.event.ActionEvent evt) {
+						btn_refreshAgentListActionPerformed(evt);
+					}
+				});
+
+		btn_openAgent.setText("Load Agent");
+		btn_openAgent.setActionCommand("btn_openAgent");
+		btn_openAgent.setName("btn_openAgent"); // NOI18N
+		btn_openAgent.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				btn_openAgentActionPerformed(evt);
+			}
+		});
+
+		javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(
+				jPanel1);
+		jPanel1.setLayout(jPanel1Layout);
+		jPanel1Layout.setHorizontalGroup(jPanel1Layout.createParallelGroup(
+				javax.swing.GroupLayout.Alignment.LEADING).addGap(0, 0,
+				Short.MAX_VALUE));
+		jPanel1Layout.setVerticalGroup(jPanel1Layout.createParallelGroup(
+				javax.swing.GroupLayout.Alignment.LEADING).addGap(0, 63,
+				Short.MAX_VALUE));
+
+		javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(
+				jPanel2);
+		jPanel2.setLayout(jPanel2Layout);
+		jPanel2Layout.setHorizontalGroup(jPanel2Layout.createParallelGroup(
+				javax.swing.GroupLayout.Alignment.LEADING).addGap(0, 0,
+				Short.MAX_VALUE));
+		jPanel2Layout.setVerticalGroup(jPanel2Layout.createParallelGroup(
+				javax.swing.GroupLayout.Alignment.LEADING).addGap(0, 129,
+				Short.MAX_VALUE));
+
+		javax.swing.GroupLayout layout = new javax.swing.GroupLayout(
+				getContentPane());
+		getContentPane().setLayout(layout);
+		layout.setHorizontalGroup(layout
+				.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+				.addGroup(
+						javax.swing.GroupLayout.Alignment.TRAILING,
+						layout.createSequentialGroup()
+								.addContainerGap()
+								.addGroup(
+										layout.createParallelGroup(
+												javax.swing.GroupLayout.Alignment.TRAILING)
+												.addComponent(
+														jPanel2,
+														javax.swing.GroupLayout.DEFAULT_SIZE,
+														javax.swing.GroupLayout.DEFAULT_SIZE,
+														Short.MAX_VALUE)
+												.addComponent(
+														jPanel1,
+														javax.swing.GroupLayout.Alignment.LEADING,
+														javax.swing.GroupLayout.DEFAULT_SIZE,
+														javax.swing.GroupLayout.DEFAULT_SIZE,
+														Short.MAX_VALUE)
+												.addGroup(
+														javax.swing.GroupLayout.Alignment.LEADING,
+														layout.createSequentialGroup()
+																.addComponent(
+																		jLabel1)
+																.addPreferredGap(
+																		javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+																.addComponent(
+																		cmb_agentList,
+																		0,
+																		255,
+																		Short.MAX_VALUE)
+																.addPreferredGap(
+																		javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+																.addComponent(
+																		btn_refreshAgentList)
+																.addPreferredGap(
+																		javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+																.addComponent(
+																		btn_openAgent)))
+								.addContainerGap()));
+		layout.setVerticalGroup(layout
+				.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+				.addGroup(
+						layout.createSequentialGroup()
+								.addContainerGap()
+								.addGroup(
+										layout.createParallelGroup(
+												javax.swing.GroupLayout.Alignment.BASELINE)
+												.addComponent(
+														jLabel1,
+														javax.swing.GroupLayout.PREFERRED_SIZE,
+														29,
+														javax.swing.GroupLayout.PREFERRED_SIZE)
+												.addComponent(
+														cmb_agentList,
+														javax.swing.GroupLayout.PREFERRED_SIZE,
+														javax.swing.GroupLayout.DEFAULT_SIZE,
+														javax.swing.GroupLayout.PREFERRED_SIZE)
+												.addComponent(
+														btn_refreshAgentList)
+												.addComponent(btn_openAgent))
+								.addPreferredGap(
+										javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+								.addComponent(jPanel1,
+										javax.swing.GroupLayout.PREFERRED_SIZE,
+										javax.swing.GroupLayout.DEFAULT_SIZE,
+										javax.swing.GroupLayout.PREFERRED_SIZE)
+								.addGap(18, 18, 18)
+								.addComponent(jPanel2,
+										javax.swing.GroupLayout.PREFERRED_SIZE,
+										javax.swing.GroupLayout.DEFAULT_SIZE,
+										javax.swing.GroupLayout.PREFERRED_SIZE)
+								.addContainerGap(44, Short.MAX_VALUE)));
+
+		cmb_agentList.getAccessibleContext().setAccessibleName("cmb_agentList");
+		btn_refreshAgentList.getAccessibleContext().setAccessibleName(
+				"btn_refreshAgentList");
+		btn_openAgent.getAccessibleContext().setAccessibleName("btn_openAgent");
+
+		pack();
+
+	}
+
+	private void refreshAgents() {
+		cmb_agentList.removeAllItems();
+		
+		String[] agents = parentAgent.getAgents();
+		for (int i = 0; i < agents.length; i++) {
+			cmb_agentList.addItem(agents[i]);
+		}
+	}
+
+	@Override
+	protected void processWindowEvent(WindowEvent e) {
+		super.processWindowEvent(e);
+
+		if (e.getID() == WindowEvent.WINDOW_CLOSING) {
+			parentAgent.doDelete();
+		}
+	}
+	
+    private void btn_refreshAgentListActionPerformed(java.awt.event.ActionEvent evt) {                                                     
+        // TODOdd your handling code here:
+    }                                                    
+
+    private void btn_openAgentActionPerformed(java.awt.event.ActionEvent evt) {                                              
+        // TODO add your handling code here:
+    }    
+
+}

87b4bf911cf2fb91bd955a53ec6c92e6840e9b9a
JC de Groot <kris.de.groot@hva.nl>
2013-12-18 13:38:47 +0100

    Monitor klasse updated

--- TCMIS/src/tcmis/mainpackage/MonitorGUI.java
+++ TCMIS/src/tcmis/mainpackage/MonitorGUI.java
@@ -6,15 +6,22 @@ package tcmis.mainpackage;
  * class and implementing the setup() method as exemplified in the code below.
  **/
 
-import java.awt.Color;
-import java.awt.Container;
-import java.awt.FlowLayout;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import java.util.Random;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.Timer;
 
 import jade.core.Agent;
 import jade.core.AID;
@@ -29,173 +36,31 @@ public class MonitorGUI extends JFrame {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	// GUI Objects
-    private javax.swing.JButton btn_openAgent;
-    private javax.swing.JButton btn_refreshAgentList;
-    private javax.swing.JComboBox cmb_agentList;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
-
 	MonitorGUI(Monitor parentAgent) {
 		this.parentAgent = parentAgent;
 
-		initComponents();
-
-		refreshAgents();
+		Overview canvas = new Overview();
+        this.setSize(1000, 500);
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.setContentPane(canvas.view);
+        this.pack();
+        this.setLocationByPlatform(true);
+        this.setVisible(true);
+        
+        //TEST
+        canvas.addCar(100, 100, 300, 300, new Color(0,190,0), "CAR_A");
+        canvas.addStation(300, 300, Color.BLACK, "A");
+        canvas.addStation(800, 200, Color.BLACK, "W");
 	}
 
-	private void initComponents() {
-		// <editor-fold defaultstate="collapsed" desc="Generated Code">
-
-		jLabel1 = new javax.swing.JLabel();
-		cmb_agentList = new javax.swing.JComboBox();
-		btn_refreshAgentList = new javax.swing.JButton();
-		btn_openAgent = new javax.swing.JButton();
-		jPanel1 = new javax.swing.JPanel();
-		jPanel2 = new javax.swing.JPanel();
-
-		setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
-
-		jLabel1.setText("Select Agent:");
-
-		cmb_agentList.setModel(new javax.swing.DefaultComboBoxModel(
-				new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-
-		btn_refreshAgentList.setText("Refresh");
-		btn_refreshAgentList.setActionCommand("R");
-		btn_refreshAgentList.setName("btn_refreshAgentList"); // NOI18N
-		btn_refreshAgentList
-				.addActionListener(new java.awt.event.ActionListener() {
-					public void actionPerformed(java.awt.event.ActionEvent evt) {
-						btn_refreshAgentListActionPerformed(evt);
-					}
-				});
-
-		btn_openAgent.setText("Load Agent");
-		btn_openAgent.setActionCommand("btn_openAgent");
-		btn_openAgent.setName("btn_openAgent"); // NOI18N
-		btn_openAgent.addActionListener(new java.awt.event.ActionListener() {
-			public void actionPerformed(java.awt.event.ActionEvent evt) {
-				btn_openAgentActionPerformed(evt);
-			}
-		});
-
-		javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(
-				jPanel1);
-		jPanel1.setLayout(jPanel1Layout);
-		jPanel1Layout.setHorizontalGroup(jPanel1Layout.createParallelGroup(
-				javax.swing.GroupLayout.Alignment.LEADING).addGap(0, 0,
-				Short.MAX_VALUE));
-		jPanel1Layout.setVerticalGroup(jPanel1Layout.createParallelGroup(
-				javax.swing.GroupLayout.Alignment.LEADING).addGap(0, 63,
-				Short.MAX_VALUE));
-
-		javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(
-				jPanel2);
-		jPanel2.setLayout(jPanel2Layout);
-		jPanel2Layout.setHorizontalGroup(jPanel2Layout.createParallelGroup(
-				javax.swing.GroupLayout.Alignment.LEADING).addGap(0, 0,
-				Short.MAX_VALUE));
-		jPanel2Layout.setVerticalGroup(jPanel2Layout.createParallelGroup(
-				javax.swing.GroupLayout.Alignment.LEADING).addGap(0, 129,
-				Short.MAX_VALUE));
-
-		javax.swing.GroupLayout layout = new javax.swing.GroupLayout(
-				getContentPane());
-		getContentPane().setLayout(layout);
-		layout.setHorizontalGroup(layout
-				.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-				.addGroup(
-						javax.swing.GroupLayout.Alignment.TRAILING,
-						layout.createSequentialGroup()
-								.addContainerGap()
-								.addGroup(
-										layout.createParallelGroup(
-												javax.swing.GroupLayout.Alignment.TRAILING)
-												.addComponent(
-														jPanel2,
-														javax.swing.GroupLayout.DEFAULT_SIZE,
-														javax.swing.GroupLayout.DEFAULT_SIZE,
-														Short.MAX_VALUE)
-												.addComponent(
-														jPanel1,
-														javax.swing.GroupLayout.Alignment.LEADING,
-														javax.swing.GroupLayout.DEFAULT_SIZE,
-														javax.swing.GroupLayout.DEFAULT_SIZE,
-														Short.MAX_VALUE)
-												.addGroup(
-														javax.swing.GroupLayout.Alignment.LEADING,
-														layout.createSequentialGroup()
-																.addComponent(
-																		jLabel1)
-																.addPreferredGap(
-																		javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-																.addComponent(
-																		cmb_agentList,
-																		0,
-																		255,
-																		Short.MAX_VALUE)
-																.addPreferredGap(
-																		javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-																.addComponent(
-																		btn_refreshAgentList)
-																.addPreferredGap(
-																		javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-																.addComponent(
-																		btn_openAgent)))
-								.addContainerGap()));
-		layout.setVerticalGroup(layout
-				.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-				.addGroup(
-						layout.createSequentialGroup()
-								.addContainerGap()
-								.addGroup(
-										layout.createParallelGroup(
-												javax.swing.GroupLayout.Alignment.BASELINE)
-												.addComponent(
-														jLabel1,
-														javax.swing.GroupLayout.PREFERRED_SIZE,
-														29,
-														javax.swing.GroupLayout.PREFERRED_SIZE)
-												.addComponent(
-														cmb_agentList,
-														javax.swing.GroupLayout.PREFERRED_SIZE,
-														javax.swing.GroupLayout.DEFAULT_SIZE,
-														javax.swing.GroupLayout.PREFERRED_SIZE)
-												.addComponent(
-														btn_refreshAgentList)
-												.addComponent(btn_openAgent))
-								.addPreferredGap(
-										javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-								.addComponent(jPanel1,
-										javax.swing.GroupLayout.PREFERRED_SIZE,
-										javax.swing.GroupLayout.DEFAULT_SIZE,
-										javax.swing.GroupLayout.PREFERRED_SIZE)
-								.addGap(18, 18, 18)
-								.addComponent(jPanel2,
-										javax.swing.GroupLayout.PREFERRED_SIZE,
-										javax.swing.GroupLayout.DEFAULT_SIZE,
-										javax.swing.GroupLayout.PREFERRED_SIZE)
-								.addContainerGap(44, Short.MAX_VALUE)));
-
-		cmb_agentList.getAccessibleContext().setAccessibleName("cmb_agentList");
-		btn_refreshAgentList.getAccessibleContext().setAccessibleName(
-				"btn_refreshAgentList");
-		btn_openAgent.getAccessibleContext().setAccessibleName("btn_openAgent");
-
-		pack();
-
-	}
-
-	private void refreshAgents() {
-		cmb_agentList.removeAllItems();
-		
-		AID[] agents = parentAgent.getAgents();
-		for (int i = 0; i < agents.length; i++) {
-			cmb_agentList.addItem(agents[i].getLocalName());
-		}
-	}
+//	private void refreshAgents() {
+//		cmb_agentList.removeAllItems();
+//		
+//		AID[] agents = parentAgent.getAgents();
+//		for (int i = 0; i < agents.length; i++) {
+//			cmb_agentList.addItem(agents[i].getLocalName());
+//		}
+//	}
 
 	@Override
 	protected void processWindowEvent(WindowEvent e) {
@@ -204,14 +69,67 @@ public class MonitorGUI extends JFrame {
 		if (e.getID() == WindowEvent.WINDOW_CLOSING) {
 			parentAgent.doDelete();
 		}
-	}
-	
-    private void btn_refreshAgentListActionPerformed(java.awt.event.ActionEvent evt) {                                                     
-    	refreshAgents();
-    }                                                    
+	}  
 
-    private void btn_openAgentActionPerformed(java.awt.event.ActionEvent evt) {                                              
-        // TODO add your handling code here:
-    }    
+}
 
+class Overview
+{
+    JLabel view;
+    BufferedImage surface;
+    Random random = new Random();
+
+    public Overview()
+    {
+        surface = new BufferedImage(1000,500,BufferedImage.TYPE_INT_RGB);
+        view = new JLabel(new ImageIcon(surface));
+        Graphics g = surface.getGraphics();
+        g.setColor(Color.WHITE);
+        g.fillRect(0,0,1000,500);
+        
+        //DRAW GARAGE
+        g.setColor(Color.BLACK);
+        g.drawString("GARAGE", 4, 16);
+        g.drawRect(-1, -1, 60, 25);
+        g.drawRect(-1, -1, 59, 24);
+        
+        g.dispose();
+    }
+
+    public void addCar(int curX, int curY, int desX, int desY, Color color, String name) {
+        Graphics g = surface.getGraphics();
+        
+        //DRAW DESTINATION
+        g.setColor(new Color(190, 190, 190));
+        g.drawLine(curX, curY, desX, desY);
+        
+        //DRAW CAR
+        g.setColor(color);
+        g.drawOval(curX - 7, curY - 7, 14, 14);
+        g.drawLine(curX - 11, curY, curX + 11, curY);
+        g.drawLine(curX, curY - 11, curX, curY + 11);
+        
+        //DRAW NAME
+        g.setColor(new Color(110, 110, 110));
+        g.drawString(name, curX + 14, curY + 14);
+        
+        g.dispose();
+        view.repaint();
+    }
+
+    public void addStation(int statX, int statY, Color color, String name) {
+        Graphics g = surface.getGraphics();
+        
+        //DRAW CAR
+        g.setColor(color);
+        g.drawOval(statX - 10, statY - 10, 20, 20);
+        g.drawOval(statX - 9, statY - 9, 18, 18);
+        
+        //DRAW NAME
+        g.setColor(new Color(0, 0, 0));
+        g.drawString(name, statX - 4, statY + 4);
+        
+        g.dispose();
+        view.repaint();
+    }
 }
